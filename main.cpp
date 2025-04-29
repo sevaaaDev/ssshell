3f565901e063e1549882e19aed1e3a1e26d6db5b
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2025-04-29 17:20:01 +0700

    prototype linewatcher

--- main.cpp
+++ main.cpp
@@ -4,21 +4,32 @@
 #include <string>
 #include <vector>
 
-std::string getline() {
-  std::string buf;
-  std::cout << "ssshell> ";
-  std::getline(std::cin, buf, '\n');
-  // TODO: if eof should exit without exec
-  // if (std::cin.eof()) {
-  //   exit(0);
-  // }
-  return buf;
-}
+// TODO: shell exit code should be accesible to other class or func
+// set it global?
+
+class Linewatcher {
+public:
+  bool isEOF_ = false;
+  std::string getline(int exitCode) {
+    std::string buf;
+    std::cout << "ssshell " << exitCode << " > ";
+    std::getline(std::cin, buf, '\n');
+    if (std::cin.eof()) {
+      isEOF_ = true;
+    }
+    return buf;
+  }
+};
 int main() {
   Executioner executioner;
+  Linewatcher linewatcher;
   bool loop = false;
   do {
-    std::string input = getline();
+    std::string input = linewatcher.getline(executioner.exitCode_);
+    if (linewatcher.isEOF_) {
+      std::cout << std::endl;
+      break;
+    }
     Parser parser(input);
     std::vector<char *> cmd = parser.getNextCmd();
     loop = executioner.exec(cmd);
