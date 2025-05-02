d967ccafe1629f178fb088caa6577e4c40eaa89d
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2025-05-02 11:18:51 +0700

    pseudo code

--- src/parser.hpp
+++ src/parser.hpp
@@ -15,3 +15,51 @@ public:
 };
 
 #endif // !PARSER_HPP
+// struct Node {
+//    std::string separator;
+//    std::vector<char*> cmd;
+//    Node left;
+//    Node right;
+// }
+//
+//          ||
+//         /  \
+//       &&    cmd
+//      /  \
+//    cmd  cmd
+//
+// && ||
+//
+//
+// execTree(node) {
+//    if (cmd == null) {
+//      execTree(node.left);
+//      if (sep is && and exit is success) {
+//         execTree(node.right);
+//      }
+//      else if (sep is || and exit is err) {
+//         execTree(node.right);
+//      }
+//      else if (sep is ;){
+//         execTree(node.right);
+//      }
+//      return;
+//    }
+//    exec(parser(cmd));
+//    return void
+// }
+//
+//
+// "ls -la && echo hell || echo worl"
+// Node ls -la
+// Node &&
+// Node echo hell
+// Node ||
+// Node echo worl
+//
+// for i = last-1; i > 0; i =i -2 {
+//    parent = v.at(i)
+//    parent.right = v.at(last)
+//    parent.left = v.at(i - 2)
+// }
+//
