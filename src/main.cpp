c3ba5d5abee249c1123546e6c4952941c5300e18
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2025-05-03 10:35:20 +0700

    command chaining && || works lets gooo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "executioner.hpp"
 #include "parser.hpp"
+#include "tree.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,19 +21,42 @@ public:
     return buf;
   }
 };
+bool execTree(Executioner &executioner, Node &node) {
+  if (node.cmd) {
+    auto cmd = Parser::parseCmd(node.cmd);
+    return executioner.exec(cmd[0], cmd);
+  }
+  bool run = execTree(executioner, *node.left);
+  if (run == false) {
+    return run;
+  }
+  if (node.separator == S_SEMICOLON) {
+    return execTree(executioner, *node.right);
+  }
+  if (node.separator == S_AND && executioner.exitCode_ == 0) {
+    return execTree(executioner, *node.right);
+  }
+  if (node.separator == S_OR && executioner.exitCode_ != 0) {
+    return execTree(executioner, *node.right);
+  }
+  return run;
+}
 int main() {
   Executioner executioner;
   Linewatcher linewatcher;
-  bool loop = false;
+  bool run = false;
   do {
     std::string input = linewatcher.getline(executioner.exitCode_);
     if (linewatcher.isEOF_) {
       std::cout << std::endl;
       break;
     }
-    Parser parser(input);
-    std::vector<char *> cmd = parser.getNextCmd();
-    loop = executioner.exec(cmd);
-  } while (loop);
+    auto vecOfCmd = Parser::parseInput(input);
+    // construct tree
+    Node *root = constructTree(vecOfCmd);
+    run = execTree(executioner, *root);
+    // TODO: we use ptr and ref randomly, please pick one or try to use it in
+    // make sense scenario
+  } while (run);
   return executioner.exitCode_;
 }
